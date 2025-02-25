b9c44ce3b675279def12da2f0ec987476ca7bfa9
ElizabethK9 <167283322+ElizabethK9@users.noreply.github.com>
2025-02-25 06:02:58 -0800

    Mid-point commit, fixed JS file to actually make it work, added some more CSS to make the website semi-responsive, got rid of a single line of code in the _layout file.

--- CharacterCreatorWebsite/wwwroot/js/site.js
+++ CharacterCreatorWebsite/wwwroot/js/site.js
@@ -1,28 +1,25 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-  $(document).ready(function() {
-    $('#sidebar').hover(function() {
-      $(this).addClass('expanded');
-    }, function() {
-      $(this).removeClass('expanded');
+// jQuery for sidebar hover effect
+$(document).ready(function () {
+    $('#sidebar').hover(function () {
+        $(this).addClass('expanded');
+    }, function () {
+        $(this).removeClass('expanded');
     });
-  });
+});
 
-  <script>
-    const toggleButton = document.getElementById('theme-toggle');
-    const currentTheme = localStorage.getItem('theme') || 'light';
+// JavaScript for theme toggle
+const toggleButton = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme') || 'light';
 
-    document.documentElement.setAttribute('data-theme', currentTheme);
+document.documentElement.setAttribute('data-theme', currentTheme);
 
-    toggleButton.addEventListener('click', () => {
-        const theme = document.documentElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
-        document.documentElement.setAttribute('data-theme', theme);
-        localStorage.setItem('theme', theme);
-    });
-</script>
+toggleButton.addEventListener('click', () => {
+    const theme = document.documentElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+});
 
-</script>
+
