dc9bff94e1ab3def47518b70525c15f8dbb13c62
ElizabethK9 <167283322+ElizabethK9@users.noreply.github.com>
2025-03-12 15:01:40 -0700

    Unknown commit

---
+++ CharacterCreatorWebsite/Models/ViewModel/CharacterChartViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace CharacterCreatorWebsite.Models.ViewModel
+{
+    public class CharacterChartViewModel
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Nickname { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string Occupation { get; set; }
+        public string Species { get; set; }
+        public string EyeColor { get; set; }
+        public List<string> Hobbies { get; set; } = new List<string>();
+        public List<string> SkillsOrTalents { get; set; } = new List<string>();
+    }
+
+}
